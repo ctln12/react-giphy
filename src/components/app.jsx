ac494ac665214a16e61ac7ce47e60c052ab4d585
Cataline Baert <snuf_kin@yahoo.com>
2019-10-07 21:46:25 +0200

    add new components

---
+++ src/components/app.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+const GIPHY_API_KEY = 'r655A3RW3xJPwN1GWesC4cAs7mYG8juv';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "Cmr1OMJ2FN0B2"
+    };
+  }
+
+  search = (query) => {
+    giphy({ apiKey: GIPHY_API_KEY, https: true})
+      .search({
+        q: query,
+        rating: 'g',
+        limit: 10
+      }, (err, result) => {
+        this.setState({
+          gifs: result.data
+        });
+      });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/gif.jsx
@@ -0,0 +1,21 @@
+/* eslint jsx-a11y/alt-text: off */
+/* eslint jsx-a11y/no-noninteractive-element-interactions: off */
+
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
+  render() {
+    const src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`;
+    return (
+      <img src={src} alt="gif" className="gif" onClick={this.handleClick} />
+    );
+  }
+}
+
+export default Gif;
